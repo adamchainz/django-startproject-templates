a7c6751235433b15b091c09fab1b6a11ce1abb91
Adam Johnson <me@adamj.eu>
2022-04-06 09:30:44 +0100

    templates dir

--- simple-core/project_name/settings.py
+++ simple-core/project_name/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "{{ secret_key }}"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
     },
 ]
